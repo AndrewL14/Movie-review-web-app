ce66cb50d78839973fc66ee3d41220d122f61063
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-04-03 20:35:46 -0500

    Created new review request

--- backend/src/main/java/movieApi/movies/dto/request/CreateReviewRequest.java
+++ backend/src/main/java/movieApi/movies/dto/request/CreateReviewRequest.java
@@ -1,2 +1,18 @@
-package movieApi.movies.dto.request;public class CreateReviewRequest {
+package movieApi.movies.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateReviewRequest {
+    private String reviewBody;
+    private double rating;
+    private String username;
+    private String movieImdbId;
+    private String userImdbId;
 }
