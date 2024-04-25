e7df6e6b5af6207d0e5fcb4fca72eebd304b7112
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-04-25 01:35:35 -0500

    added Role object

--- backend/src/main/java/movieApi/movies/entity/Role.java
+++ backend/src/main/java/movieApi/movies/entity/Role.java
@@ -1,2 +1,25 @@
-package movieApi.movies.entity;public class Role {
+package movieApi.movies.entity;
+
+import lombok.Data;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+
+@Setter
+@Data
+@Document(collection = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    private ObjectId roleId;
+    private String authority;
+
+    public Role() {
+        super();
+    }
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
 }
