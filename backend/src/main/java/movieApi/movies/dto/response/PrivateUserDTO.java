e267b975ae16ce7ff86a12fa14d4f75c16012bc5
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-04-25 01:37:40 -0500

    added JWT variable

--- backend/src/main/java/movieApi/movies/dto/response/PrivateUserDTO.java
+++ backend/src/main/java/movieApi/movies/dto/response/PrivateUserDTO.java
@@ -1,16 +1,17 @@
 package movieApi.movies.dto.response;
 
+import lombok.Builder;
 import movieApi.movies.entity.Review;
 
 import java.util.List;
-
+@Builder
 public record PrivateUserDTO(
         String imdbId,
         String firstName,
         String lastName,
         String username,
-        String password,
         String email,
+        String jwt,
         List<Review> userReviews
 ) {
 }
