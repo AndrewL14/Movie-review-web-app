15f27dddfc5c678756f72dcd5133c76d8392f9ed
Andrew Lam <andrew.x.li.lam@gmail.com>
2024-04-25 01:38:48 -0500

    Added RSA keys

--- backend/src/main/java/movieApi/movies/utils/KeyGeneratorUtility.java
+++ backend/src/main/java/movieApi/movies/utils/KeyGeneratorUtility.java
@@ -1,2 +1,25 @@
-package movieApi.movies.utils;public class KeyGeneratorUtility {
+package movieApi.movies.utils;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+public class KeyGeneratorUtility {
+    /**
+     * Creates a new RSA key pair by generating a private and public key.
+     * @return RSA keyPair.
+     */
+    public static KeyPair generateRsaKey(){
+
+        KeyPair keyPair;
+
+        try{
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch(Exception e){
+            throw new IllegalStateException();
+        }
+
+        return keyPair;
+    }
 }
